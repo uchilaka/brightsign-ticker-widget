09256983cb39291e134b8d3bc4de8fc8da1cf036
Uchenna Chilaka <uchenna.chilaka@alliancedata.com>
2018-09-14 17:28:46 -0400

    added raf polyfill

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 // Backward compatibility
 import "core-js/es6/map";
 import "core-js/es6/set";
+import "raf/polyfill";
 
+// Bootstrap
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
